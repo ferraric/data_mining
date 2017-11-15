aef86e086151e641b63f5043b0a534dd9db77043
Claudio Ferrari <claudioferrari@public-docking-cx-0417.ethz.ch>
2017-11-15 09:37:50 +0100

    bla

--- Task2/claudio_sol/project2.py
+++ Task2/claudio_sol/project2.py
@@ -2,39 +2,57 @@ import numpy as np
 import time
 
 # m is the number of dimensions in our final representation
-m = 4000
+m = 8000
 # d is the number of input dimensions
 d = 400
+sqr = np.sqrt(2.0 / m)
 
 # draw m samples from p(omega), and b
-b = np.random.uniform(0,2.0*np.pi,m)
+b = np.random.uniform(0,2.0*np.pi,m).astype('float32')
 # every row is one sample
 #omega = np.random.multivariate_normal(np.zeros(d),np.identity(d),m)
-omega = np.random.laplace(size=(m,d))
+#omega = np.random.laplace(size=(m,d)).astype('float32')
+omega = np.random.exponential(scale=1,size=(m,d))
 #omega = np.random.standard_cauchy(size=(m,d))
 
+
+def fourier_transform(x):
+    fourier_feature = np.dot(omega, x)
+    fourier_feature += b
+    np.cos(fourier_feature, fourier_feature)
+    fourier_feature *= sqr
+
+    return fourier_feature.astype('float32')
+
 def transform(X):
-    # Make sure this function works for both 1D and 2D NumPy arrays.
-    if X.ndim == 1:
-        X_new = np.zeros(m)
-        for j in range(m):
-            X_new[j] = np.cos(np.dot(omega[j,:],X)+b[j])
-        X_new = np.sqrt(2.0/m)*X_new
-    elif X.ndim ==2:
-        n = X.shape[0]
-
-        X_new = np.zeros((n,m))
-
-        for i in range(n):
-            for j in range(m):
-                X_new[i,j] = np.cos(np.dot(omega[j,:],X[i,:])+b[j])
-
-        X_new = np.sqrt(2.0/m)*X_new
+    if len(X.shape) == 1:
+        return fourier_transform(X)
     else:
-        X_new = 0
-        print("ERROR, transform method can only deal with 1d or 2d arrays")
+        # Make sure this function works for both 1D and 2D NumPy arrays.
+        return np.array(map(lambda x: fourier_transform(np.array(x)), X))
 
-    return X_new
+# def transform(X):
+#     # Make sure this function works for both 1D and 2D NumPy arrays.
+#     if X.ndim == 1:
+#         X_new = np.zeros(m)
+#         for j in range(m):
+#             X_new[j] = np.cos(np.dot(omega[j,:],X)+b[j])
+#         X_new = np.sqrt(2.0/m)*X_new
+#     elif X.ndim ==2:
+#         n = X.shape[0]
+#
+#         X_new = np.zeros((n,m))
+#
+#         for i in range(n):
+#             for j in range(m):
+#                 X_new[i,j] = np.cos(np.dot(omega[j,:],X[i,:])+b[j])
+#
+#         X_new = np.sqrt(2.0/m)*X_new
+#     else:
+#         X_new = 0
+#         print("ERROR, transform method can only deal with 1d or 2d arrays")
+#
+#     return X_new
 
 
 def mapper(key, value):
@@ -44,38 +62,61 @@ def mapper(key, value):
     images = value
     n = len(images)
     # numpy matrix to hold our values
-    X = np.zeros((n,d))
-    y = np.zeros(n)
-    i = 0
-    for im in images:
-        features = im.split(' ')
-        y[i] = features.pop(0)
-        X [i,:] = features
-        i += 1
-
+    data = np.array(map(lambda x: np.array(x.split(" "), dtype='float32'), value))
+    np.random.shuffle(data)
 
     #initialize w
     w = np.zeros(m)
-    #w = np.random.normal(scale=1.0, size=m)
+
     # regularization parameter
-    C = 1000
-
-    # generate random permutation of 1..n
-    perm = np.random.permutation(n)
-
+    C = 100
 
     stepsize = 1.0
     t = 1.0
-    for i in perm:
-        X_new = transform(X[i,:])
-        stepsize = 2.0/np.sqrt(t)
+    for d in data:
+        y = d[0]
+        x = transform(d[1:])
+        stepsize = 2/np.sqrt(t)
         t += 1.0
-        if y[i]*np.dot(w,X_new) >= 1:
+        if y*np.dot(w,x) >= 1:
             w = w - stepsize/n * w
             #pass
         else:
-            w = w - stepsize*(w*1.0/n-C*y[i]*X_new)
-            #w = w - stepsize*y[i]*X_new[i,:]
+            w = w - stepsize*(w*1.0/n-C*y*x)
+            # w = w - stepsize*y*x
+
+    # X = np.zeros((n,d))
+    # y = np.zeros(n)
+    # i = 0
+    # for im in images:
+    #     features = im.split(' ')
+    #     y[i] = features.pop(0)
+    #     X [i,:] = features
+    #     i += 1
+    #
+    #
+    # #initialize w
+    # w = np.zeros(m)
+    # #w = np.random.normal(scale=1.0, size=m)
+    # # regularization parameter
+    # C = 10000
+    #
+    # # generate random permutation of 1..n
+    # perm = np.random.permutation(n)
+    #
+    #
+    # stepsize = 1.0
+    # t = 1.0
+    # for i in perm:
+    #     X_new = transform(X[i,:])
+    #     stepsize = 0.1
+    #     t += 1.0
+    #     if y[i]*np.dot(w,X_new) >= 1:
+    #         w = w - stepsize/n * w
+    #         #pass
+    #     else:
+    #         w = w - stepsize*(w*1.0/n-C*y[i]*X_new)
+    #         # w = w - stepsize*y[i]*X_new
 
     end = time.time()
     print("Mapper time: " + str(end - start))
