76b62a447ead79fd2806b18f23d66508c829562b
demircis <demirci.sinan96@gmail.com>
2017-12-04 16:47:48 +0100

    take mean of all points as initial center

--- Task3/sinan_sol/project3_k-meansII.py
+++ Task3/sinan_sol/project3_k-meansII.py
@@ -7,7 +7,6 @@ feature_dimension = 250
 def mapper(key, value):
     # key: None
     # value: one line of input file
-    np.random.shuffle(value)
     yield "key", value
 
 
@@ -16,13 +15,13 @@ def reducer(key, values):
     # values: list of all value for that key
     # Note that we do *not* output a (key, value) pair here.
     start = time.time()
-    #np.random.shuffle(values)
+    np.random.shuffle(values)
     # number of images
     k = values.shape[0]
     # array containing 200 centers for initialization of the result (random)
     result = np.zeros((nr_total_centers,feature_dimension))
     # pick the first center randomly (shuffled)
-    result[0,:] = values[np.random.choice(range(k)),:]
+    result[0,:] = np.sum(values,axis=0) / k
     # D holds the distances from datapoints to closest center
     D = np.zeros(k)
     # psi is the sum of distances from the centers (sum of values in D)
@@ -32,6 +31,7 @@ def reducer(key, values):
     # oversampling factor and nr of iterations (we get about l*n centers)
     n = 7
     l = 30
+
     # start k-means|| (initialization)
     start_barbar = time.time()
     for i in range(n):
@@ -50,7 +50,7 @@ def reducer(key, values):
         for p in range(k):
             # random value between 0 and 1
             ind = np.random.random_sample()
-            # if probability (l*D[i]/psi) is high enough, sample as center
+            # if probability (l*D[p]/psi) is high enough, sample as center
             if ind <= l*D[p]/psi:
                 result[r,:] = values[p,:]
                 r += 1
@@ -77,7 +77,7 @@ def reducer(key, values):
                 c = dist
                 min_index = j
         # weigh higher distances more than lower ones
-        if c > 10 and i < k*l-5000:
+        if c > 10 and i < k*l-6000:
             stepsize = 1.0
         elif c == 0:
             continue
